c2ed03882f3a60675805da5900fa3f075c2c6917
Amalia <amaliarvm@gmail.com>
2019-07-17 10:45:26 +0200

    Updates

---
+++ precache-manifest.6c0d4e03e4e90c1e7850c239fa05fee3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0c090e30e96f8a9695ebb3f1b1babb5",
+    "url": "/TestForm/index.html"
+  },
+  {
+    "revision": "3d0473f3a97bcf5f6959",
+    "url": "/TestForm/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "91be1471f25943ecc480",
+    "url": "/TestForm/static/js/2.b3250fa9.chunk.js"
+  },
+  {
+    "revision": "3d0473f3a97bcf5f6959",
+    "url": "/TestForm/static/js/main.90a9349d.chunk.js"
+  },
+  {
+    "revision": "bd59c62a22e47adb338e",
+    "url": "/TestForm/static/js/runtime~main.6ecaa6da.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TestForm/static/media/logo.5d5d9eef.svg"
+  }
+]);
